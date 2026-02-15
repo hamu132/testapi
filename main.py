bb98b803dda2f6288563b2b4244c0bbdded94975
Mogura <guba06120@gmail.com>
2026-02-15 04:09:58 +0000

    ほえ～

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 # 1. 名前を追加する (POSTのような操作)
 print("--- ユーザーを追加します ---")
-response_add = requests.get("http://localhost:8080/add", params={"user": "Pythonくん"})
+response_add = requests.get("http://localhost:8080/add", params={"user": "ハム太郎"})
 print(response_add.json()["message"])
 
 # 2. 一覧を取得する
