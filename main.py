ef65051c815a0c632d46c7e9d02c216e7d55a955
Mogura <guba06120@gmail.com>
2026-02-15 04:06:43 +0000

    aaa

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+# 1. 名前を追加する (POSTのような操作)
+print("--- ユーザーを追加します ---")
+response_add = requests.get("http://localhost:8080/add", params={"user": "Pythonくん"})
+print(response_add.json()["message"])
+
+# 2. 一覧を取得する
+print("\n--- 現在のリストを取得します ---")
+response_list = requests.get("http://localhost:8080/list")
+data = response_list.json()
+
+print(f"メッセージ: {data['message']}")
+for i, name in enumerate(data['data']):
+    print(f"{i+1}人目: {name}")
